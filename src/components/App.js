62f0eb2c09a964128a859502b45d2a821970fe69
Brian Pham <brianlpham21@gmail.com>
2020-02-26 23:08:17 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          New Page
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App;
