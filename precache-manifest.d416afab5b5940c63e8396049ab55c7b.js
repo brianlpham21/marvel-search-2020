346008991a20044484479147fc4e8d7ea308cf0e
Brian Pham <brianlpham21@gmail.com>
2020-02-29 23:16:39 -0800

    Updates

---
+++ precache-manifest.d416afab5b5940c63e8396049ab55c7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddb5350c281fb232cedab95b085fd46f",
+    "url": "/marvel-search-2020/index.html"
+  },
+  {
+    "revision": "dc265f373ffd1b0d67f8",
+    "url": "/marvel-search-2020/static/css/main.e3069ce7.chunk.css"
+  },
+  {
+    "revision": "b680c53322f35a75ae07",
+    "url": "/marvel-search-2020/static/js/2.f1e83873.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/marvel-search-2020/static/js/2.f1e83873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc265f373ffd1b0d67f8",
+    "url": "/marvel-search-2020/static/js/main.1e45ed7b.chunk.js"
+  },
+  {
+    "revision": "502e3be7fd57a3fd6942",
+    "url": "/marvel-search-2020/static/js/runtime-main.8efccad5.js"
+  },
+  {
+    "revision": "39ec8218672bd40ad1edcbc1bd2761ca",
+    "url": "/marvel-search-2020/static/media/Marvel_Animation_Logo.39ec8218.jpg"
+  },
+  {
+    "revision": "addb15046873315f8c4614171d46bc49",
+    "url": "/marvel-search-2020/static/media/Marvel_Comic_BG.addb1504.jpg"
+  }
+]);
